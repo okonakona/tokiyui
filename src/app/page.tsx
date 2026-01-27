50186f06b158547dabc100e64ae06ea06b1019d4
okonakona <2240057@ecc.ac.jp>
2026-01-27 21:32:49 +0900

    styles: レイアウトの調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,28 @@ import styles from "./style.module.css";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.content}>
       <header>
-        <h1><Image src="/assets/logo.svg" alt="時結-tokiyui-AR"  width={342} height={173}/></h1>
+        <h1><Image src="/assets/logo.svg" alt="時結-tokiyui-AR"  width={342} height={245}/></h1>
       </header>
       <main>
-        <Link href="/ar">ARを始める</Link>
-        <Link href="/how-to">遊び方</Link>
+        <Link href="/ar" className={styles.arBtn}>　ARを始める</Link>
+        <Link href="/how-to" className={styles.howtoBtn}>遊び方</Link>
       </main>
       <footer>
-        <Link href="">記録</Link>
-        <Link href="">見つける</Link>
+        <Link href="/record" className={styles.footerBtn}>
+        <div className={styles.box}>
+          <Image src="/assets/record.svg" alt="写真のアイコン" width={35} height={35} />
+        </div>
+        記録
+        </Link>
+        <Link href="/search" className={styles.footerBtn}>
+        <div className={styles.box}>
+          <Image src="/assets/search.svg" alt="虫眼鏡のアイコン" width={35} height={35} />
+        </div>
+        発見
+        </Link>
       </footer>
-    </>
+    </div>
   );
 }
