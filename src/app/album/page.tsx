f62b1fee56f0e1e2d4129d7a7bc5155c89f8f893
okonakona <2240057@ecc.ac.jp>
2026-02-12 02:42:49 +0900

    fix: タブでコンテンツの切り替えを可能に

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -11,6 +11,8 @@ type Record = {
 
 export default function AlbumPage() {
     const [image, setImage] = useState<string | null>(null);
+    const [activeTab, setActiveTab] = useState<"people" | "poem">("people");
+
 
     useEffect(() => {
         const saved = localStorage.getItem("lastShot");
@@ -23,139 +25,87 @@ export default function AlbumPage() {
                 <h1>図鑑</h1>
                 <section>
                     <ul className={styles.nav}>
-                        <li className={styles.people}>
+                        <li
+                            className={styles.people}
+                            onClick={() => setActiveTab("people")}
+                        >
                             <h2>人物</h2>
                         </li>
-                        <li className={styles.poem}>
+                        <li
+                            className={styles.poem}
+                            onClick={() => setActiveTab("poem")}
+                        >
                             <h2>和歌</h2>
                         </li>
                     </ul>
                 </section>
             </header>
             <main>
-                <div className={styles.content}>
-                    <div className={styles.container}>
-                        <div className={styles.wrapper}>
-                            <Link href="ar/explain" >
-                                <div className={styles.box}>
-                                    {image && (
-                                        <img
-                                            src={image}
-                                            alt="保存した画像"
-                                            className={styles.previewImage}
-                                        />
-                                    )}
+                <div className={`${styles.content} ${activeTab === "people"
+                    ? styles.contentPeople
+                    : styles.contentPoem
+                    }`}>
+                    {activeTab === "people" && (
+                        <>
+                            <div className={styles.container}>
+                                <div className={styles.wrapper}>
+                                    <Link href="ar/explain" >
+                                        <div className={styles.box}>
+                                            {image && (
+                                                <img
+                                                    src={image}
+                                                    alt="保存した画像"
+                                                    className={styles.previewImage}
+                                                />
+                                            )}
+                                        </div>
+                                    </Link>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
                                 </div>
-                            </Link>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                        </div>
-                        <div className={styles.textWrap}>
-                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
-                            <h3>織田信長</h3>
-                        </div>
-                    </div>
-                    <div className={styles.container}>
-                        <div className={styles.wrapper}>
-                            <Link href="ar/explain" >
-                                <div className={styles.box}>
-                                    {image && (
-                                        <img
-                                            src={image}
-                                            alt="保存した画像"
-                                            className={styles.previewImage}
-                                        />
-                                    )}
+                                <div className={styles.textWrap}>
+                                    <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
+                                    <h3>織田信長</h3>
                                 </div>
-                            </Link>
-                            <div className={styles.box}>
                             </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                        </div>
-                        <div className={styles.textWrap}>
-                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
-                            <h3>織田信長</h3>
-                        </div>
-                    </div>
-                    <div className={styles.container}>
-                        <div className={styles.wrapper}>
-                            <Link href="ar/explain" >
-                                <div className={styles.box}>
-                                    {image && (
-                                        <img
-                                            src={image}
-                                            alt="保存した画像"
-                                            className={styles.previewImage}
-                                        />
-                                    )}
+                        </>
+                    )}
+                    {activeTab === "poem" && (
+                        <>
+                            <div className={styles.container}>
+                                <div className={styles.wrapper}>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
+                                    <div className={styles.box}>
+                                    </div>
                                 </div>
-                            </Link>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                        </div>
-                        <div className={styles.textWrap}>
-                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
-                            <h3>織田信長</h3>
-                        </div>
-                    </div>
-                    <div className={styles.container}>
-                        <div className={styles.wrapper}>
-                            <Link href="ar/explain" >
-                                <div className={styles.box}>
-                                    {image && (
-                                        <img
-                                            src={image}
-                                            alt="保存した画像"
-                                            className={styles.previewImage}
-                                        />
-                                    )}
+                                <div className={styles.textWrap}>
+                                    <p>百人の和歌を一人につき一首ずつ選んで作られた百首から成る秀歌撰（詞華集）。</p>
+                                    <h3>百人一首</h3>
                                 </div>
-                            </Link>
-                            <div className={styles.box}>
                             </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                        </div>
-                        <div className={styles.textWrap}>
-                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
-                            <h3>織田信長</h3>
-                        </div>
-                    </div>
+                        </>
+                    )}
                 </div>
 
-            </main>
+            </main >
             <footer>
                 <Link href="/" className={styles.backBtn}>戻る</Link>
             </footer>
-        </div>
+        </div >
     );
 }
