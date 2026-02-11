6b91ee90a313e327281c44612b150e6db4248098
okonakona <2240057@ecc.ac.jp>
2026-02-12 02:24:40 +0900

    styles: 増減に対応するスタイル調整

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -19,25 +19,22 @@ export default function AlbumPage() {
 
     return (
         <div>
-            <header>
-                <li>
-                    <h2>人物</h2>
-                </li>
-                <li>
-                    <h2>和歌</h2>
-                </li>
+            <header className={styles.header}>
+                <h1>図鑑</h1>
+                <section>
+                    <ul className={styles.nav}>
+                        <li className={styles.people}>
+                            <h2>人物</h2>
+                        </li>
+                        <li className={styles.poem}>
+                            <h2>和歌</h2>
+                        </li>
+                    </ul>
+                </section>
             </header>
             <main>
                 <div className={styles.content}>
                     <div className={styles.container}>
-                        <div className={styles.wrapper}>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                            <div className={styles.box}>
-                            </div>
-                        </div>
                         <div className={styles.wrapper}>
                             <Link href="ar/explain" >
                                 <div className={styles.box}>
@@ -54,9 +51,102 @@ export default function AlbumPage() {
                             </div>
                             <div className={styles.box}>
                             </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
                         </div>
                         <div className={styles.textWrap}>
-                            <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
+                            <h3>織田信長</h3>
+                        </div>
+                    </div>
+                    <div className={styles.container}>
+                        <div className={styles.wrapper}>
+                            <Link href="ar/explain" >
+                                <div className={styles.box}>
+                                    {image && (
+                                        <img
+                                            src={image}
+                                            alt="保存した画像"
+                                            className={styles.previewImage}
+                                        />
+                                    )}
+                                </div>
+                            </Link>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                        </div>
+                        <div className={styles.textWrap}>
+                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
+                            <h3>織田信長</h3>
+                        </div>
+                    </div>
+                    <div className={styles.container}>
+                        <div className={styles.wrapper}>
+                            <Link href="ar/explain" >
+                                <div className={styles.box}>
+                                    {image && (
+                                        <img
+                                            src={image}
+                                            alt="保存した画像"
+                                            className={styles.previewImage}
+                                        />
+                                    )}
+                                </div>
+                            </Link>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                        </div>
+                        <div className={styles.textWrap}>
+                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
+                            <h3>織田信長</h3>
+                        </div>
+                    </div>
+                    <div className={styles.container}>
+                        <div className={styles.wrapper}>
+                            <Link href="ar/explain" >
+                                <div className={styles.box}>
+                                    {image && (
+                                        <img
+                                            src={image}
+                                            alt="保存した画像"
+                                            className={styles.previewImage}
+                                        />
+                                    )}
+                                </div>
+                            </Link>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                            <div className={styles.box}>
+                            </div>
+                        </div>
+                        <div className={styles.textWrap}>
+                            <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
                             <h3>織田信長</h3>
                         </div>
                     </div>
