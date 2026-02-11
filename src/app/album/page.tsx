5ffec15804a34757fda94864ed0403152718400e
okonakona <2240057@ecc.ac.jp>
2026-02-12 02:52:28 +0900

    fix: 配列で管理できるよう修正。仮データ

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -9,6 +9,16 @@ type Record = {
     savedAt: number;
 };
 
+const albumData = {
+    people: [
+        { title: "織田信長", text: "日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。" },
+        { title: "豊臣秀吉", text: "三英傑の一人。織田信長の後を継いで天下統一を果たし、近世封建社会の基礎を築いた。" },
+    ],
+    poem: [
+        { title: "百人一首", text: "百人の和歌を一人につき一首ずつ選んで作られた百首から成る秀歌撰（詞華集）。" },
+    ],
+};
+
 export default function AlbumPage() {
     const [image, setImage] = useState<string | null>(null);
     const [activeTab, setActiveTab] = useState<"people" | "poem">("people");
@@ -45,7 +55,7 @@ export default function AlbumPage() {
                     ? styles.contentPeople
                     : styles.contentPoem
                     }`}>
-                    {activeTab === "people" && (
+                    {albumData[activeTab].map((item, index) => (
                         <>
                             <div className={styles.container}>
                                 <div className={styles.wrapper}>
@@ -72,34 +82,12 @@ export default function AlbumPage() {
                                     </div>
                                 </div>
                                 <div className={styles.textWrap}>
-                                    <p>日本の戦国時代から安土桃山時代にかけての武将・大名。戦国の三英傑の一人。 尾張国出身。</p>
-                                    <h3>織田信長</h3>
+                                    <p>{item.text}</p>
+                                    <h3>{item.title}</h3>
                                 </div>
                             </div>
                         </>
-                    )}
-                    {activeTab === "poem" && (
-                        <>
-                            <div className={styles.container}>
-                                <div className={styles.wrapper}>
-                                    <div className={styles.box}>
-                                    </div>
-                                    <div className={styles.box}>
-                                    </div>
-                                    <div className={styles.box}>
-                                    </div>
-                                    <div className={styles.box}>
-                                    </div>
-                                    <div className={styles.box}>
-                                    </div>
-                                </div>
-                                <div className={styles.textWrap}>
-                                    <p>百人の和歌を一人につき一首ずつ選んで作られた百首から成る秀歌撰（詞華集）。</p>
-                                    <h3>百人一首</h3>
-                                </div>
-                            </div>
-                        </>
-                    )}
+                    ))}
                 </div>
 
             </main >
